b569d056b5155d24b2a6cf06f4fc811798c24a28
dicksen <dicksen.veloopillay@telecom.mu>
2020-08-12 22:11:05 +0400

    gesture detector and flag hideZero added

--- lib/icon_badge.dart
+++ lib/icon_badge.dart
@@ -3,61 +3,74 @@ library icon_badge;
 import 'package:flutter/material.dart';
 
 class IconBadge extends StatelessWidget {
-  final IconData iconData;
+  final Icon icon;
   final VoidCallback onTap;
   final int itemCount;
-  final bool showZero;
+  final bool hideZero;
   final Color badgeColor;
   final double top;
   final double right;
-  final double left;
-  final double bottom;
 
   const IconBadge({
     Key key,
     this.onTap,
-    @required this.iconData,
-    this.itemCount,
-    this.showZero,
+    @required this.icon,
+    this.itemCount = 0,
+    this.hideZero = false,
     this.badgeColor,
     this.top = 5.0,
     this.right = 6.0,
-    this.left,
-    this.bottom,
   }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return InkWell(
-      onTap: onTap,
-      child: Container(
-        width: 72,
-        padding: const EdgeInsets.symmetric(horizontal: 8),
-        child: Stack(
-          alignment: Alignment.center,
-          children: [
-            Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                Icon(iconData),
-              ],
-            ),
-            Positioned(
-              top: top,
-              right: right,
-              left: left == null ? 0 : left,
-              bottom: bottom == null ? 0 : bottom,
-              child: Container(
-                padding: EdgeInsets.symmetric(horizontal: 6, vertical: 2),
-                decoration:
-                    BoxDecoration(shape: BoxShape.circle, color: badgeColor),
+    return itemCount == 0 && hideZero
+        ? GestureDetector(
+            onTap: onTap,
+            child: Container(
+              width: 72,
+              padding: const EdgeInsets.symmetric(horizontal: 8),
+              child: Stack(
                 alignment: Alignment.center,
-                child: Text('$itemCount'),
+                children: [
+                  Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      icon,
+                    ],
+                  ),
+                ],
               ),
-            )
-          ],
-        ),
-      ),
-    );
+            ),
+          )
+        : GestureDetector(
+            onTap: onTap,
+            child: Container(
+              width: 72,
+              padding: const EdgeInsets.symmetric(horizontal: 8),
+              child: Stack(
+                alignment: Alignment.center,
+                children: [
+                  Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      icon,
+                    ],
+                  ),
+                  Positioned(
+                    top: top,
+                    right: right,
+                    child: Container(
+                      padding: EdgeInsets.symmetric(horizontal: 6, vertical: 2),
+                      decoration: BoxDecoration(
+                          shape: BoxShape.circle, color: badgeColor),
+                      alignment: Alignment.center,
+                      child: Text('$itemCount'),
+                    ),
+                  )
+                ],
+              ),
+            ),
+          );
   }
 }
